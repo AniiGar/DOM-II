16cadeb53a5ed60300300ffa73c35cb86b1889dd
Anita Garris <aniigarris@gmail.com>
2019-11-02 18:02:56 -0600

    Created 3 event listeners. This is a work in progress...

--- js/index.js
+++ js/index.js
@@ -1,1 +1,38 @@
-// Your code goes here
+// Click
+const heading = document.querySelector('.logo-heading');
+
+heading.addEventListener('click', (object) => { 
+
+    let busName = ()=> {
+        let getName = prompt('Name that bus!', 'Enter bus name')
+        return getName;
+    };
+    object.target.textContent = busName();
+});
+
+// Mouseover
+const subs4 = document.querySelectorAll('h4');
+
+subs4.forEach((object)=> {
+    object.addEventListener('mouseover', (object) => { 
+        object.target.style.textDecoration = "underline";
+    });
+});
+
+// Doubleclick
+const aTag = document.querySelectorAll('a');
+
+aTag.forEach((object)=> {
+    object.addEventListener('dblclick', (object)=>{
+        object.target.style.color = 'blue';
+    });
+  });
+
+        // Alternate code 
+            // function eachTag(object) {
+            //     object.addEventListener('dblclick', (object)=>{
+            //             object.target.style.color = 'blue';
+            //     });
+            //   }
+
+            // aTag.forEach(eachTag);
